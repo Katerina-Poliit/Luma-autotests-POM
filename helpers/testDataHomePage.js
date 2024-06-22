525188b27e192e357eda65609c6502b87e3a4e90
AlexUA2022 <avsaponov@gmail.com>
2024-06-22 10:52:19 +0300

    RF_POM | homePage.spec | ТС 03.1.5 Verify that the "Men" navigation menu link is displayed and contains the appropriate text

--- helpers/testDataHomePage.js
+++ helpers/testDataHomePage.js
@@ -7,3 +7,4 @@ export const BASE_URL = 'https://magento.softwaretestingboard.com';
 
 export const WHATS_NEW_LINK_TEXT = "What's New";
 export const WOMEN_LINK_TEXT = 'Women';
+export const MEN_LINK_TEXT = 'Men';
