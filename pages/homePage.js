578d14ef1898ea9812c9090b91ed78959958c16b
Katerina Poliit <alexispoliyit@gmail.com>
2024-06-23 18:09:54 +0300

    RF_POM | homePage.spec |ТС 03.1.117 Verify that the "Women" navigation menu link contains the dropdown, ТС 03.1.119 Verify that the dropdown contains the "Tops" dropdown, ТС 03.1.120 Verify that the "Tops" dropdown contains a cursor pointer, ТС 03.1.121 Verify that the "Tops" dropdown redirects to the appropriate pages, ТС 03.1.122 Verify that the "Tops" dropdown contains the "Jackets" subcategory, ТС 03.1.123 Verify that the "Jackets" subcategory contains a cursor pointer

--- pages/homePage.js
+++ pages/homePage.js
@@ -9,39 +9,39 @@ import { WhatsNewPage } from "./watsNewPage";
 import { WomenPage } from "./womenPage";
 import NotesPage from "./notesPage";
 import ParticeAPIPage from "./particeApi.Page";
-import WriteForUsPage  from "./writeForUsPage";
+import WriteForUsPage from "./writeForUsPage";
 import SubscribePage from "./subscribePage";
 import PolicyPage from "./policyPage";
 import SearchtermsPage from "./searchTermsPage";
 import AndvancedSearchPage from "./advancedSearchPage";
-import ResultSearchPage from "./resultSearchPage";
 import OrdersReturnsPage from "./ordersReturnsPage";
+import WomenTopsPage from "./womenTopsPage";
 
 export class HomePage {
 
-   constructor(page) {
+	constructor(page) {
 
-        this.page = page;
-      //----------------------------- Header -----------------------------------------------------------
-        this.headerSection = page.getByText('Toggle Nav My Cart My Cart');
-		  this.storeLogo = page.getByLabel('store logo');
-		  this.signInlink = page.getByRole('link', { name: 'Sign In' });
-		  this.createAnAccountLink = page.getByRole('banner').getByRole('link', { name: 'Create an Account' });
-		  this.searchField = page.getByPlaceholder('Search entire store here...');
-		  this.searchBtn = page.locator("button[title='Search']");
-		  this.dropdownSearch = page.locator('#search_autocomplete > ul > li');
-		  this.dropdownItem = page.locator('.qs-option-name').filter({hasText: 'shorts for men'});
-		  this.cartBtn = page.getByRole('link', { name: ' My Cart' });
+		this.page = page;
+		//----------------------------- Header -----------------------------------------------------------
+		this.headerSection = page.getByText('Toggle Nav My Cart My Cart');
+		this.storeLogo = page.getByLabel('store logo');
+		this.signInlink = page.getByRole('link', { name: 'Sign In' });
+		this.createAnAccountLink = page.getByRole('banner').getByRole('link', { name: 'Create an Account' });
+		this.searchField = page.getByPlaceholder('Search entire store here...');
+		this.searchBtn = page.locator("button[title='Search']");
+		this.dropdownSearch = page.locator('#search_autocomplete > ul > li');
+		this.dropdownItem = page.locator('.qs-option-name').filter({ hasText: 'shorts for men' });
+		this.cartBtn = page.getByRole('link', { name: ' My Cart' });
 		//----------------------------- Footer -----------------------------------------------------------
-         this.footerContent = this.page.locator('.page-wrapper footer'),
-		 this.noteslink = page.getByRole('link', { name: 'Notes' }),
-		 this.pacticeAPI = page.getByRole('link', { name: 'Practice API Testing using Magento' }),
-		 this.forUsLink = page.getByRole('link', { name: 'Write for us' });
-		 this.subscribeLink = page.getByRole('link', { name: 'Subscribe' });
-		 this.privacyCookiesLink = page.getByRole('link', { name: 'Privacy and Cookie Policy' });
-		 this.searchTermsLink = page.getByRole('link', { name: 'Search Terms' });
-		 this.advancedSearchLink = page.getByRole('link', { name: 'Advanced Search' });
-		 this.ordersReturnsLink = page.getByRole('link', { name: 'Orders and Returns' });
+		this.footerContent = this.page.locator('.page-wrapper footer'),
+			this.noteslink = page.getByRole('link', { name: 'Notes' }),
+			this.pacticeAPI = page.getByRole('link', { name: 'Practice API Testing using Magento' }),
+			this.forUsLink = page.getByRole('link', { name: 'Write for us' });
+		this.subscribeLink = page.getByRole('link', { name: 'Subscribe' });
+		this.privacyCookiesLink = page.getByRole('link', { name: 'Privacy and Cookie Policy' });
+		this.searchTermsLink = page.getByRole('link', { name: 'Search Terms' });
+		this.advancedSearchLink = page.getByRole('link', { name: 'Advanced Search' });
+		this.ordersReturnsLink = page.getByRole('link', { name: 'Orders and Returns' });
 
 
 
@@ -54,9 +54,13 @@ export class HomePage {
 		this.trainingLink = page.getByRole('menuitem', { name: 'Training' });
 		this.saleLink = page.getByRole('menuitem', { name: 'Sale' });
 
+		//----------------------------- Kate -----------------------------------------------------------
+		this.womenDropdown = page.locator('.nav-2 > ul > li > a');
+		this.womenCategoryTops = page.getByRole('menuitem', { name: ' Tops' });
+		this.womenTopsJackets = page.locator('#ui-id-11');
 
 
-   }
+	}
 
 	async open() {
 		await this.page.goto("/");
@@ -175,6 +179,21 @@ export class HomePage {
 	async clickOrdersReturnsLink() {
 		await this.ordersReturnsLink.click();
 		return new OrdersReturnsPage(this.page);
+	}
+
+	async hoverwomenLink() {
+		await this.womenLink.hover();
+	}
+
+	async clickwomenCategoryTops() {
+		await this.womenCategoryTops.click();
+
+		return new WomenTopsPage(this.page);
+	}
+
+	async hoverwomenCategoryTops() {
+		await this.womenCategoryTops.hover();
+
 	}
 
 
--- tests/homePage.spec.js
+++ tests/homePage.spec.js
@@ -1,5 +1,5 @@
 const { test, expect } = require('@playwright/test');
-import { BASE_URL, WHATS_NEW_LINK_TEXT, WOMEN_LINK_TEXT, MEN_LINK_TEXT, GEAR_LINK_TEXT, TRAINING_LINK_TEXT, SALE_LINK_TEXT, WHATS_NEW_URL, WHATS_NEW_PAGE_HEADER_TEXT, WOMEN_URL, WOMEN_PAGE_HEADER_TEXT, MEN_URL, MEN_PAGE_HEADER_TEXT, GEAR_URL, GEAR_PAGE_HEADER_TEXT, TRAINING_URL, TRAINING_PAGE_HEADER_TEXT, SALE_URL, SALE_PAGE_HEADER_TEXT } from "../helpers/testDataHomePage";
+import { BASE_URL, WHATS_NEW_LINK_TEXT, WOMEN_LINK_TEXT, MEN_LINK_TEXT, GEAR_LINK_TEXT, TRAINING_LINK_TEXT, SALE_LINK_TEXT, WHATS_NEW_URL, WHATS_NEW_PAGE_HEADER_TEXT, WOMEN_URL, WOMEN_PAGE_HEADER_TEXT, MEN_URL, MEN_PAGE_HEADER_TEXT, GEAR_URL, GEAR_PAGE_HEADER_TEXT, TRAINING_URL, TRAINING_PAGE_HEADER_TEXT, SALE_URL, SALE_PAGE_HEADER_TEXT,WOMEN_CATAGORY_TOPS, WOMEN_TOPS_PAGE_URL, WOMEN_TOPS_SUBCATEGORY_JACKETS, WOMEN_TOPS_SUBCATEGORY_HOODIESSWEATSHIRTS, WOMEN_TOPS_JACKETS_URL,  WOMEN_TOPS_SUBCATEGORY_HOODIESSWEATSHIRTS_URL, PANTS_URL, PANTS_HEADER_TEXT, OFF_20_SECTION_HEADER_TEXT, OFF_20_SECTION_CTA_TEXT, SHOP_PANTS_LINK_TEXT, HEADING_TOPS_TEXT, HEADING_JACKETS_TEXT, HEADING_HOODIES_TEXT, WOMEN_TOPS_SUBCATEGORY_TEES_TEXT, WOMEN_TOPS_TEES_URL, HEADING_TEES_PAGE_TEXT, WOMEN_TOPS_SUBCATEGORY_BRAS_TANKS, WOMEN_TOPS_BRASSTANKS_URL, HEADING_BRASTANKS_PAGE_TEXT, WOMEN_CATAGORY_BOTTOMS, WOMEN_BOTTOMS_PAGE_URL, HEADING_BOTTOMS_TEXT, WOMEN_BOTTOMS_SUBCATEGORY_PANTS_TEXT, WOMEN_BOTTOMS_PANTS_PAGE_URL, HEADING_WOMEN_PANTS_PAGE_TEXT, WOMEN_BOTTOMS_SUBCATEGORY_SHORTS_TEXT, WOMEN_SHORTS_PAGE_URL, HEADING_WOMEN_SHORTS_PAGE_TEXT, MEN_DROPDOWN_TEXT, MEN_CATAGORY_TOPS_TEXT, MEN_TOPS_PAGE_URL, HEADING_MEN_TOPS_PAGE_TEXT, MEN_TOPS_SUBCATEGORY_JACKETS, MEN_TOPS_JACKETS_URL, MEN_TOPS_HOODIESSWEATSHIRTS_TEXT, MEN_HOODIESSWEATSHIRTS_URL, MEN_TOPS_TEES_TEXT, TEES_URL, TEES_HEADER_TEXT, EVEN_MORE_SECTION_HEADER_TEXT, EVEN_MORE_SECTION_CTA_TEXT, SHOP_TEES_LINK_TEXT, MEN_TOPS_TESS_PAGE_URL, MEN_TOPS_SUBCATEGORY_TANKS_TEXT, MEN_TOPS_TANKS_PAGE_URL, HEADING_MEN_TANKS_PAGE_TEXT,  MEN_CATAGORY_BOTTOMS, MEN_BOTTOMS_PAGE_URL,MEN_BOTTOMS_SUBCATEGORY_PANTS_TEXT, MEN_BOTTOMS_PANTS_PAGE_URL, HEADING_MEN_BOTTOMS_PANTS_TEXT, MEN_SHORTS_PAGE_URL, HEADING_MEN_SHORTS_PAGE_TEXT, GEAR_BAGS_PAGE_URL, HEADING_GEAR_BAGS_PAGE_TEXT, GEAR_FITNESS_PAGE_URL, GEAR_WATCHES_PAGE_URL, TRAINING_VIDEO_DOWNLOAD_PAGE_URL } from "../helpers/testDataHomePage";
 import { HomePage } from "../pages/homePage";
 
 test.describe('homePage.spec', () => {
@@ -188,4 +188,56 @@ test.describe('homePage.spec', () => {
 
 	});
 
-})
+	test('ТС 03.1.117 Verify that the "Women" navigation menu link contains the dropdown', async ({ page }) => {
+
+		await homePage.hoverwomenLink();
+	    expect(homePage.womenDropdown).toBeTruthy();
+
+	});
+
+	test('ТС 03.1.119 Verify that the dropdown contains the "Tops" dropdown', async ({ page }) => {
+
+		await homePage.hoverwomenLink();
+		await expect(homePage.womenCategoryTops).toHaveText(WOMEN_CATAGORY_TOPS);
+
+	});
+
+	test('ТС 03.1.120 Verify that the "Tops" dropdown contains a cursor pointer', async ({ page }) => {
+
+		await homePage.hoverwomenLink();
+		await expect(homePage.womenCategoryTops).toHaveCSS('cursor', 'pointer');
+
+	});
+
+	test('ТС 03.1.121 Verify that the "Tops" dropdown redirects to the appropriate pages', async ({ page }) => {
+
+		await homePage.hoverwomenLink();
+		const womenTopsPage = await homePage.clickwomenCategoryTops();
+		await expect(page).toHaveURL(WOMEN_TOPS_PAGE_URL);
+		await expect(womenTopsPage.headingPage).toBeVisible();
+		await expect(womenTopsPage.headingPage).toHaveText(HEADING_TOPS_TEXT);
+
+	});
+
+	test('ТС 03.1.122 Verify that the "Tops" dropdown contains the "Jackets" subcategory', async ({ page }) => {
+
+		const homePage = new HomePage(page);
+
+		await homePage.hoverwomenLink();
+		await homePage.hoverwomenCategoryTops();
+		await expect(homePage.womenTopsJackets).toBeVisible();
+		await expect(homePage.womenTopsJackets).toHaveText(WOMEN_TOPS_SUBCATEGORY_JACKETS)
+
+	});
+
+	test('ТС 03.1.123 Verify that the "Jackets" subcategory contains a cursor pointer', async ({ page }) => {
+
+		const homePage = new HomePage(page);
+
+		await homePage.hoverwomenLink();
+		await homePage.hoverwomenCategoryTops();
+		await expect(homePage.womenTopsJackets).toHaveCSS('cursor', 'pointer');
+
+	});
+
+});
