dd21d2f0f7fbc5b158f3037758dc8db3a2e31e28
Katerina Poliit <alexispoliyit@gmail.com>
2024-06-23 13:51:13 +0300

    RF_POM | footerPage.spec | ТС 02.1.27 Verify that the "Advanced Search" link opens the page, the user clicks on the "Advanced Search" link, ТС 02.1.27.01 Verify that the "Advanced Search" page contains the search settings, ТС 02.1.28 Verify that the "Advanced Search" page contains the "Product Name" field, ТС 02.1.29 Verify that the "Advanced Search" page contains the "SKU" field, ТС 02.1.30 Verify that the "Advanced Search" page contains the "Description" field

--- pages/advancedSearchPage.js
+++ pages/advancedSearchPage.js
@@ -1,5 +1,9 @@
 export default class AndvancedSearchPage {
     constructor (page) {
-        this.page = page
+        this.page = page;
+        this.settingsSearch = page.locator('legend');
+        this.productNameField = page.getByLabel('Product Name');
+        this.SKUField = page.getByLabel('SKU');
+        this.descriptionField = page.getByLabel('Description', { exact: true });
     }
 }
