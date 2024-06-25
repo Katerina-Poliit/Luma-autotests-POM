d116d98f892590b3914f890e77b97b44398004f4
Alexander Saponov <avsaponov@gmail.com>
2024-06-25 09:55:40 +0300

    RF_POM | homePage.spec | ТС 03.1.80 Verify that the "Hot Sellers" section contains six product cards (#122)

--- pages/homePage.js
+++ pages/homePage.js
@@ -97,6 +97,7 @@ export class HomePage {
 		this.hotSellersSection = page.locator('.widget-product-grid');
 		this.hotSellersSectionHeader = page.getByRole('heading', { name: 'Hot Sellers' });
 		this.hotSellersSectionInfText = page.getByText('Here is what`s trending on');
+		this.hotSellersProductCsrdsItem = page.locator('.product-item-info');
 
 		//----------------------------- Kate -----------------------------------------------------------
 		this.womenDropdown = page.locator('.nav-2 > ul > li > a');
