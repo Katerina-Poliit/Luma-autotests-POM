973c5d87d5067d1b60748d5d14f68f2f57936e44
Katerina Poliit <alexispoliyit@gmail.com>
2024-06-25 17:07:59 +0300

    RF_POM | homePage.spec | ТС 03.1.172 Verify that the "Training" navigation menu link contains the dropdown, ТС 03.1.173 Verify that the dropdown contains a cursor pointer, ТС 03.1.175 Verify that the "Video download"subcategory is redirected to the appropriate page

---
+++ pages/trainingVideoDownloadPage.js
@@ -0,0 +1,6 @@
+export default class TrainingVideoDownloadPage {
+    constructor (page) {
+        this.page = page;
+        this.Heading = this.page.getByLabel('Video Download').getByText('Video Download');
+    }
+}
