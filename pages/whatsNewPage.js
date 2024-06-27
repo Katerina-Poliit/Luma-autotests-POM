12aed3843afc8edb9664fcbea2e6ce3581096f39
Alexander Saponov <avsaponov@gmail.com>
2024-06-27 11:49:30 +0300

    RF_POM | whatsNewPage.spec | ТС 04.1.2 Verify that the "What\'s New" page contains the header, ТС 04.1.3 Verify that the "What\'s New" page contains the "NEW IN WOMEN\'S" section, ТС 04.1.4 Verify that the "NEW IN WOMEN\'S" section contains the header, ТС 04.1.5 Verify that the "NEW IN WOMEN\'S" section contains the "Hoodies & Sweatshirts" link, ТС 04.1.6 Verify that the "Hoodies & Sweatshirts" link has a cursor pointer (#152)

--- pages/whatsNewPage.js
+++ pages/whatsNewPage.js
@@ -7,7 +7,10 @@ export class WhatsNewPage {
         this.page = page;
 
 		  this.header = page.getByLabel('What\'s New').getByText('What\'s New');
-		  this.homeBreadcrumbs = page.getByRole('link', { name: 'Home' })
+		  this.homeBreadcrumbs = page.getByRole('link', { name: 'Home' });
+		  this.newInWomansSection = page.getByText('Hoodies & Sweatshirts Jackets Tees Bras & Tanks Pants Shorts');
+		  this.newInWomansSectionHeader = page.getByText('New in women\'s');
+		  this.hoodiesSweatshirtsLink = page.getByRole('link', { name: 'Hoodies & Sweatshirts' }).first()
 
    }
 
