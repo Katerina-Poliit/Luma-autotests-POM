39c9d8858b2ee46bcc06a0bbcc3e24e4908c52af
Alexander Saponov <avsaponov@gmail.com>
2024-06-27 12:52:13 +0300

    RF_POM | whatsNewPage.spec | ТС 04.1.7 Verify that the user is redirected to the "Hoodies & Sweatshirts" page after clicking on the "Hoodies & Sweatshirts" link, ТС 04.1.8 Verify that the "NEW IN WOMEN\'S" section contains the "Jackets" link, ТС 04.1.9 Verify that the "Jackets" link has a cursor pointer, ТС 04.1.10 Verify that the user is redirected to the "Jackets" page after clicking on the "Jackets" link, ТС 04.1.11 Verify that the "NEW IN WOMEN\'S" section contains the "Tees" link (#154)

--- helpers/testDataWhatsNewPage.js
+++ helpers/testDataWhatsNewPage.js
@@ -2,6 +2,8 @@
 
 export const BASE_URL = 'https://magento.softwaretestingboard.com';
 export const WHATS_NEW_URL = 'https://magento.softwaretestingboard.com/what-is-new.html';
+export const HOODIES_SWEATSHIRTS_URL = 'https://magento.softwaretestingboard.com/women/tops-women/hoodies-and-sweatshirts-women.html';
+export const JACKETS_URL = 'https://magento.softwaretestingboard.com/women/tops-women/jackets-women.html';
 
 //test data
 
@@ -9,3 +11,9 @@ export const HOME_PAGE_BREADCRUMBS = 'Home';
 export const WHATS_NEW_HEADER_TEXT = "What's New";
 export const NEW_IN_WOMANS_SECTION_HEADER_TEXT = "New in women's";
 export const HOODIES_SWEATSHIRTS_LINK_TEXT = 'Hoodies & Sweatshirts';
+export const HOODIES_SWEATSHIRTS_HEADER_TEXT = 'Hoodies & Sweatshirts';
+export const HOODIES_SWEATSHIRTS_BREADCRUMBS_TEXT = 'Hoodies & Sweatshirts';
+export const JACKETS_LINK_TEXT = 'Jackets';
+export const JACKETS_BREADCRUMBS_TEXT = 'Jackets';
+export const JACKETS_HEADER_TEXT = 'Jackets';
+export const TEES_LINK_TEXT = 'Tees';
--- tests/whatsNewPage.spec.js
+++ tests/whatsNewPage.spec.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 import { HomePage } from "../pages/homePage";
 
-import { BASE_URL, WHATS_NEW_URL, HOME_PAGE_BREADCRUMBS, WHATS_NEW_HEADER_TEXT, NEW_IN_WOMANS_SECTION_HEADER_TEXT, HOODIES_SWEATSHIRTS_LINK_TEXT } from "../helpers/testDataWhatsNewPage";
+import { BASE_URL, WHATS_NEW_URL, HOME_PAGE_BREADCRUMBS, WHATS_NEW_HEADER_TEXT, NEW_IN_WOMANS_SECTION_HEADER_TEXT, HOODIES_SWEATSHIRTS_LINK_TEXT,  HOODIES_SWEATSHIRTS_HEADER_TEXT, HOODIES_SWEATSHIRTS_URL, HOODIES_SWEATSHIRTS_BREADCRUMBS_TEXT, JACKETS_URL, JACKETS_LINK_TEXT, JACKETS_BREADCRUMBS_TEXT, JACKETS_HEADER_TEXT, TEES_LINK_TEXT } from "../helpers/testDataWhatsNewPage";
 
 test.describe('whatsNewPage.spec', () => {
 
@@ -28,7 +28,7 @@ test.describe('whatsNewPage.spec', () => {
 			await expect(whatsNewPage.header).toBeVisible();
 			await expect(whatsNewPage.header).toHaveText(WHATS_NEW_HEADER_TEXT);
 
-			await whatsNewPage.clickhomeBreadcrumbs(); 
+			await whatsNewPage.clickHomeBreadcrumbs(); 
 
 			await expect(page).toHaveURL(BASE_URL);
 			await expect(whatsNewPage.header).not.toBeVisible();
@@ -66,6 +66,55 @@ test.describe('whatsNewPage.spec', () => {
 
 		await expect(whatsNewPage.hoodiesSweatshirtsLink).toBeVisible();
 		await expect(whatsNewPage.hoodiesSweatshirtsLink).toHaveCSS('cursor', 'pointer');
+		
+	});
+
+	test('ТС 04.1.7 Verify that the user is redirected to the "Hoodies & Sweatshirts" page after clicking on the "Hoodies & Sweatshirts" link', async ({ page }) => {
+
+		const hoodiesSweatshirtsLink = await whatsNewPage.clickHoodiesSweatshirtsLink();
+
+		await expect(page).toHaveURL(HOODIES_SWEATSHIRTS_URL);
+
+		await expect(hoodiesSweatshirtsLink.hoodiesSweatshirtsBreadcrumbs).toBeVisible();
+		await expect(hoodiesSweatshirtsLink.hoodiesSweatshirtsBreadcrumbs).toHaveText(HOODIES_SWEATSHIRTS_BREADCRUMBS_TEXT);
+
+		await expect(hoodiesSweatshirtsLink.header).toBeVisible();
+		await expect(hoodiesSweatshirtsLink.header).toHaveText(HOODIES_SWEATSHIRTS_HEADER_TEXT);
+		
+	});
+
+	test('ТС 04.1.8 Verify that the "NEW IN WOMEN\'S" section contains the "Jackets" link', async ({ page }) => {
+
+		await expect(whatsNewPage.jacketsLink).toBeVisible();
+		await expect(whatsNewPage.jacketsLink).toHaveText(JACKETS_LINK_TEXT);
+		
+	});
+
+	test('ТС 04.1.9 Verify that the "Jackets" link has a cursor pointer', async ({ page }) => {
+
+		await expect(whatsNewPage.jacketsLink).toBeVisible();
+		await expect(whatsNewPage.jacketsLink).toHaveCSS('cursor', 'pointer');
+		
+	});
+
+	test('ТС 04.1.10 Verify that the user is redirected to the "Jackets" page after clicking on the "Jackets" link', async ({ page }) => {
+
+		const hoodiesSweatshirtsLink = await whatsNewPage.clickJacketsLink();
+
+		await expect(page).toHaveURL(JACKETS_URL);
+
+		await expect(hoodiesSweatshirtsLink.jacketsBreadcrumbs).toBeVisible();
+		await expect(hoodiesSweatshirtsLink.jacketsBreadcrumbs).toHaveText(JACKETS_BREADCRUMBS_TEXT);
+
+		await expect(hoodiesSweatshirtsLink.header).toBeVisible();
+		await expect(hoodiesSweatshirtsLink.header).toHaveText(JACKETS_HEADER_TEXT);
+		
+	});
+
+	test('ТС 04.1.11 Verify that the "NEW IN WOMEN\'S" section contains the "Tees" link', async ({ page }) => {
+
+		await expect(whatsNewPage.teesLink).toBeVisible();
+		await expect(whatsNewPage.teesLink).toHaveText(TEES_LINK_TEXT);
 		
 	});
 
