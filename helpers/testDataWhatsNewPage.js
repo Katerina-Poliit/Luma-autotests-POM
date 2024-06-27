669087e27dd78f5ca6f02c2338b9695aaf17a903
AlexUA2022 <avsaponov@gmail.com>
2024-06-27 11:45:28 +0300

    RF_POM | whatsNewPage.spec | ТС 04.1.2 Verify that the "What\'s New" page contains the header, ТС 04.1.3 Verify that the "What\'s New" page contains the "NEW IN WOMEN\'S" section, ТС 04.1.4 Verify that the "NEW IN WOMEN\'S" section contains the header, ТС 04.1.5 Verify that the "NEW IN WOMEN\'S" section contains the "Hoodies & Sweatshirts" link, ТС 04.1.6 Verify that the "Hoodies & Sweatshirts" link has a cursor pointer

--- helpers/testDataWhatsNewPage.js
+++ helpers/testDataWhatsNewPage.js
@@ -7,3 +7,5 @@ export const WHATS_NEW_URL = 'https://magento.softwaretestingboard.com/what-is-n
 
 export const HOME_PAGE_BREADCRUMBS = 'Home';
 export const WHATS_NEW_HEADER_TEXT = "What's New";
+export const NEW_IN_WOMANS_SECTION_HEADER_TEXT = "New in women's";
+export const HOODIES_SWEATSHIRTS_LINK_TEXT = 'Hoodies & Sweatshirts';
--- tests/whatsNewPage.spec.js
+++ tests/whatsNewPage.spec.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 import { HomePage } from "../pages/homePage";
 
-import { BASE_URL, WHATS_NEW_URL, HOME_PAGE_BREADCRUMBS, WHATS_NEW_HEADER_TEXT } from "../helpers/testDataWhatsNewPage";
+import { BASE_URL, WHATS_NEW_URL, HOME_PAGE_BREADCRUMBS, WHATS_NEW_HEADER_TEXT, NEW_IN_WOMANS_SECTION_HEADER_TEXT, HOODIES_SWEATSHIRTS_LINK_TEXT } from "../helpers/testDataWhatsNewPage";
 
 test.describe('whatsNewPage.spec', () => {
 
@@ -18,20 +18,55 @@ test.describe('whatsNewPage.spec', () => {
         whatsNewPage = await homePage.clickWatsNewLink();
     });
 
-    test('ТС 04.1.1 Verify that the user can navigate by breadcrumbs', async ({ page }) => {
+	test('ТС 04.1.1 Verify that the user can navigate by breadcrumbs', async ({ page }) => {
 
-        await expect(page).toHaveURL(WHATS_NEW_URL); 
-		  await expect(whatsNewPage.homeBreadcrumbs).toBeVisible();
-		  await expect(whatsNewPage.homeBreadcrumbs).toHaveCSS('cursor', 'pointer');
-		  await expect(whatsNewPage.homeBreadcrumbs).toHaveText(HOME_PAGE_BREADCRUMBS);
+			await expect(page).toHaveURL(WHATS_NEW_URL); 
+			await expect(whatsNewPage.homeBreadcrumbs).toBeVisible();
+			await expect(whatsNewPage.homeBreadcrumbs).toHaveCSS('cursor', 'pointer');
+			await expect(whatsNewPage.homeBreadcrumbs).toHaveText(HOME_PAGE_BREADCRUMBS);
 
-		  await expect(whatsNewPage.header).toBeVisible();
-		  await expect(whatsNewPage.header).toHaveText(WHATS_NEW_HEADER_TEXT);
+			await expect(whatsNewPage.header).toBeVisible();
+			await expect(whatsNewPage.header).toHaveText(WHATS_NEW_HEADER_TEXT);
 
-        await whatsNewPage.clickhomeBreadcrumbs(); 
+			await whatsNewPage.clickhomeBreadcrumbs(); 
 
-        await expect(page).toHaveURL(BASE_URL);
-		  await expect(whatsNewPage.header).not.toBeVisible();
-    });
+			await expect(page).toHaveURL(BASE_URL);
+			await expect(whatsNewPage.header).not.toBeVisible();
+
+	});
+
+	test('ТС 04.1.2 Verify that the "What\'s New" page contains the header', async ({ page }) => {
+
+			await expect(whatsNewPage.header).toBeVisible();
+			await expect(whatsNewPage.header).toHaveText(WHATS_NEW_HEADER_TEXT);
+			
+	});
+
+	test('ТС 04.1.3 Verify that the "What\'s New" page contains the "NEW IN WOMEN\'S" section', async ({ page }) => {
+
+		await expect(whatsNewPage.newInWomansSection).toBeVisible();
+		
+	});
+
+	test('ТС 04.1.4 Verify that the "NEW IN WOMEN\'S" section contains the header', async ({ page }) => {
+
+		await expect(whatsNewPage.newInWomansSectionHeader).toBeVisible();
+		await expect(whatsNewPage.newInWomansSectionHeader).toHaveText(NEW_IN_WOMANS_SECTION_HEADER_TEXT);
+		
+	});
+
+	test('ТС 04.1.5 Verify that the "NEW IN WOMEN\'S" section contains the "Hoodies & Sweatshirts" link', async ({ page }) => {
+
+		await expect(whatsNewPage.hoodiesSweatshirtsLink).toBeVisible();
+		await expect(whatsNewPage.hoodiesSweatshirtsLink).toHaveText(HOODIES_SWEATSHIRTS_LINK_TEXT);
+		
+	});
+
+	test('ТС 04.1.6 Verify that the "Hoodies & Sweatshirts" link has a cursor pointer', async ({ page }) => {
+
+		await expect(whatsNewPage.hoodiesSweatshirtsLink).toBeVisible();
+		await expect(whatsNewPage.hoodiesSweatshirtsLink).toHaveCSS('cursor', 'pointer');
+		
+	});
 
 });
